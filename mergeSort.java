a62428bf6b87fd6414b92245320881fcff176bb6
ctanenbaum <113850552+ctanenbaum@users.noreply.github.com>
2023-02-19 22:27:53 -0500

    commented file

--- mergeSort.java
+++ mergeSort.java
@@ -6,25 +6,25 @@ public class mergeSort<T extends Comparable<T>> {
 
     public static <T extends Comparable<T>> void merge(int leftFirst, int leftLast, int rightFirst, int rightLast, T[] array){
         T[] tempArray = Arrays.copyOf(array,array.length);
-        int index = leftFirst;
-        int saveFirst = leftFirst;
-
+        int index = leftFirst; //set index to start from left
+        int saveFirst = leftFirst; 
+        //keep comparing until each element is split
         while((leftFirst <= leftLast) && (rightFirst <= rightLast)){
-            if(array[leftFirst].compareTo(array[rightFirst]) < 0){
-                tempArray[index] = array[leftFirst];
-                leftFirst++;
+            if(array[leftFirst].compareTo(array[rightFirst]) < 0){ //if the left element is less than one on right
+                tempArray[index] = array[leftFirst];               //save it in the next index in tempArray
+                leftFirst++;                                       /keep moving along...
             }else{
                 tempArray[index] = array[rightFirst];
                 rightFirst++;
             }
             index++;
         }
-        while(leftFirst <= leftLast){
+        while(leftFirst <= leftLast){  //compare the left side of the split
             tempArray[index] = array[leftFirst];
             leftFirst++;
             index++;
         }
-        while(rightFirst <= rightLast){
+        while(rightFirst <= rightLast){  //compare the right side of the split
             tempArray[index] = array[rightFirst];
             rightFirst++;
             index++;
@@ -35,6 +35,7 @@ public class mergeSort<T extends Comparable<T>> {
     }
     public static <T extends Comparable<T>> void mergeSort(int first, int last,T[] array){
         if(first < last){
+            //recursive call to keep merging
             int middle = (first + last) / 2;
             mergeSort(first,middle,array);
             mergeSort(middle+1,last,array);
