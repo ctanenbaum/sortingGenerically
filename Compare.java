f444ec30e6e3835338db8d484e652af23152ff05
ctanenbaum <113850552+ctanenbaum@users.noreply.github.com>
2023-02-19 22:15:37 -0500

    commented file

--- Compare.java
+++ Compare.java
@@ -3,7 +3,7 @@ package sortingGenerically;
 import java.util.Comparator;
 
 public class Compare<T extends Comparable<T>> implements Comparator<T> {
-
+        //generic comparator class that is using the compare method (not the compareTo)
 	@Override
 	public int compare(T shoe1, T shoe2) {
 		
